c556992d52638784dd7010ea0f9bf307e0e51717
Gene Lee <gwlee244@gmail.com>
2020-03-03 20:31:05 -0500

    adding initial files to repo

---
+++ test/Letter.test.js
@@ -0,0 +1,27 @@
+const Letter = require("../lib/Letter");
+
+describe("Letter class", () => {
+  it("Characters that aren't digits or letters are instantly visible", () => {
+    
+  });
+
+  it("toString returns _ for letters", () => {
+    
+  });
+
+  describe("guess", () => {
+    it("Correct guess returns true", () => {
+      
+    });
+
+    it("Incorrect guess returns false", () => {
+      
+    });
+  });
+
+  describe("getSolution", () => {
+    it("returns character", () => {
+      
+    });
+  });
+});
---
+++ test/Word.test.js
@@ -0,0 +1,22 @@
+const Word = require("../lib/Word");
+
+describe("Word class", () => {
+  describe("guessLetter", () => {
+    it("Correct guess returns true", () => {
+      
+    });
+
+    it("Incorrect guess returns false", () => {
+      
+    });
+  });
+
+  describe("guessedCorrectly ", () => {
+    it("returns true if all letters are correct", () => {
+
+    });
+    it("returns false if at least one letter is incorrect", () => {
+
+    });
+  });
+});
